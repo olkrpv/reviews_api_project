aa22786abb09e3959c8c0749b20ac37ea9a30cf8
Ольга Карпова <tordvist@yandex.ru>
2023-09-08 10:25:30 +0300

    Fix pep8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -47,7 +47,7 @@ class TitleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Title
         fields = ('id', 'name', 'year', 'description', 'category', 'genre')
-    
+
     def to_representation(self, instance):
         serializer = TitleGETSerializer(instance)
         return serializer.data
